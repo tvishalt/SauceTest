9fbe21110f0ad9db985d639a0d6cb7c2a218d05b
Vishal Tamhankar <vishal.tamhankar@lntinfotech.com>
2016-08-24 15:20:34 +0530

    test

--- SauceEvaluation/src/com/sauceevaluation/PageBase.java
+++ SauceEvaluation/src/com/sauceevaluation/PageBase.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 /**
  * Created by mehmetgerceker on 12/7/15.
  */
-public class PageBase {
+public class PageBase {//
 	
     protected static void setCheckCheckBoxState(WebElement checkBox, boolean checked)
             throws InvalidElementStateException {
